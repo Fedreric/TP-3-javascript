873da93b4c285e17cb2ac70d72170b112f343b32
Fedreric <116675592+Fedreric@users.noreply.github.com>
2023-03-13 14:25:53 -0300

    se agrega funcion para dibujar la tabla

--- js/ejercicio7.js
+++ js/ejercicio7.js
@@ -1,5 +1,35 @@
+function generarTabla(num) {
+    document.write(`<h1>Tabla del ${num}</h1>`);
+    document.write(`<table border="1">
+                           <tbody>`);                 
+    for (let i = 1; i <= 10; i++) {
+        document.write('<tr>');
+        document.write(`<td style="
+                            padding: 10px;"
+                            >${num} x ${i}
+                            </td>`)
+        document.write(`<td style="
+                            padding: 10px;"
+                            >${num * i}
+                            </td>`);
+        document.write('</tr>');
+    }
+
+    document.write(`    </tbody>
+                    </table>`);
+}
+
+
+
+
+
+
+
+
+
+
+
+
 const num = parseInt(prompt('Ingresa un numero'));
 
-for(let i = 1; i <= 10; i++){
-    document.write(`${num} x ${i} = ${num * i} <br>`);
-}
+generarTabla(num);
